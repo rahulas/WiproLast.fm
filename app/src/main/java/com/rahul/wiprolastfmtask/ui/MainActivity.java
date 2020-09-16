9e062cb2b5c542d316eebf7e8087d54d431a2da2
Rahul <rahul@nhancenow.com>
2020-09-16 21:54:54 +0530

    Initial commit

---
+++ app/src/main/java/com/rahul/wiprolastfmtask/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rahul.wiprolastfmtask.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rahul.wiprolastfmtask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
